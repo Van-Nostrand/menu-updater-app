0716cff16ca1b7a13ccadd3a7cd8037d34d1cd6c
Van-Nostrand <dmdoull43@gmail.com>
2020-12-28 21:17:32 -0800

    having serious issues getting this running...

--- index.js
+++ index.js
@@ -4,13 +4,50 @@ const bodyParser = require("body-parser");
 const PORT = 8000;
 const axios = require("axios");
 const path = require("path");
+const mongoose = require("mongoose");
 
+const MONGODB_CURRENT_IP = '192.168.0.107';
+const MONGODB_CURRENT_PORT = '27017';
+const MONGODB_DB_NAME = "restaurant";
+
+const MONGODB_OPTIONS = {
+  // db: { native_parser: true },
+  // server: { poolSize: 5 },
+  // replset: { rs_name: 'myReplicaSetName' },
+  user: 'restauranteditor',
+  pass: '12345678',
+  useNewUrlParser: true,
+  keepAlive: true,
+  useUnifiedTopology: true
+  // promiseLibrary: global.Promise
+}
+const url = `mongodb://${MONGODB_CURRENT_IP}:${MONGODB_CURRENT_PORT}/${MONGODB_DB_NAME}`;
+
+mongoose.connect(url, MONGODB_OPTIONS);
+
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log("h");
+});
+
+exports.test = function(req,res) {
+  console.log(res)
+};
+
+// console.log(mongoose);
 
 app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.static(path.join(__dirname,"/src")));
 
 app.get("/", function(req,res){
   res.sendFile("index.html");
+});
+
+app.post("/create-beer", function(req,res){
+  let parameters = req.params;
+  res.send(parameters);
 });
 
 app.listen(PORT, () => {
---
+++ models/beer.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const beerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxLength: 100,
+    unique: true
+  },
+  tapnumber: {
+    type: Number,
+    required: false
+  },
+  pour1: {
+    type: Number,
+    required: true
+  },
+  pour1price: {
+    type: Number,
+    required: true
+  },
+  pour2: {
+    type: Number,
+    required: false
+  },
+  pour2price: {
+    type: Number,
+    required: false
+  }, 
+  pour3: {
+    type: Number,
+    required: false
+  },
+  pour3price: {
+    type: Number,
+    required: false
+  }
+},
+{
+  timestamps: true
+});
+
+beerSchema.methods.editBeerData = async function(data, next){
+  try{
+    if(data.name) this.name = data.name;
+    if(data.tapnumber) this.tapnumber = data.tapnumber;
+    if(data.pour1) this.pour1 = data.pour1;
+    if(data.pour1price) this.pour1price = data.pour1price;
+    if(data.pour2) this.pour2 = data.pour2;
+    if(data.pour2price) this.pour2price = data.pour2price;
+    if(data.pour3) this.pour3 = data.pour3;
+    if(data.pour3price) this.pour3price = data.pour3price;
+  }catch(err){
+    return next(err);
+  }
+};
+
+const Beer = mongoose.model("Beer", beerSchema);
+module.exports = Beer;

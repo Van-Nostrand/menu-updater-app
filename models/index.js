c9c8405c87497ae2d82215cf0ab67c9aba8c6188
Van-Nostrand <dmdoull43@gmail.com>
2021-01-04 23:26:25 -0800

    now connects to mongodb cloud

--- models/index.js
+++ models/index.js
@@ -15,7 +15,8 @@ const MONGODB_OPTIONS = {
   useCreateIndex: true
 }
 // const URL = `mongodb://${MONGODB_CURRENT_IP}:${MONGODB_CURRENT_PORT}/${MONGODB_DB_NAME}`;
-const URL = `mongodb://localhost:${MONGODB_CURRENT_PORT}/${MONGODB_DB_NAME}`;
+// const URL = `mongodb://localhost:${MONGODB_CURRENT_PORT}/${MONGODB_DB_NAME}`;
+const URL = `mongodb+srv://databaseManager:9Zhn7npViUeCW98@cluster0.pchwe.mongodb.net/Cluster0?retryWrites=true&w=majority`;
 
 mongoose.connect(
   process.env.MONGODB_URI || URL,

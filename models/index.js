eda75d33ca082551f6507a510aeedb4a317f07fd
Van-Nostrand <dmdoull43@gmail.com>
2021-01-05 14:11:10 -0800

    quick commit before pull merge

--- models/index.js
+++ models/index.js
@@ -6,16 +6,16 @@ const MONGODB_CURRENT_IP = '192.168.0.105';
 const MONGODB_CURRENT_PORT = '27017';
 const MONGODB_DB_NAME = "restaurant";
 const MONGODB_OPTIONS = {
-  // user: 'restauranteditor',
-  // pass: '12345678',
+  user: 'restauranteditor',
+  pass: '12345678',
   dbName: "restaurant",
   useNewUrlParser: true,
   keepAlive: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 }
-// const URL = `mongodb://${MONGODB_CURRENT_IP}:${MONGODB_CURRENT_PORT}/${MONGODB_DB_NAME}`;
-const URL = `mongodb://localhost:${MONGODB_CURRENT_PORT}/${MONGODB_DB_NAME}`;
+const URL = `mongodb://${MONGODB_CURRENT_IP}:${MONGODB_CURRENT_PORT}/${MONGODB_DB_NAME}`;
+// const URL = `mongodb://localhost:${MONGODB_CURRENT_PORT}/${MONGODB_DB_NAME}`;
 
 mongoose.connect(
   process.env.MONGODB_URI || URL,
